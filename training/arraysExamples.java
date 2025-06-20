3c27bfdcc85b07c29b05ec6714d90d7fcc7cbaf3
Pavan Kumar Chiluveru <chipavan@gmail.com>
2025-06-20 21:50:52 +0530

    Removed sys outs

--- training/arraysExamples.java
+++ training/arraysExamples.java
@@ -5,12 +5,13 @@ public class arraysExamples {
     public static void main(String args[]) {
 
         int[] a = {21,32,43,34,55,16,87,58,29};         // default array size is 10.
-        System.out.println("val..."+a[3]);
+        //System.out.println("val..."+a[3]);
 
         String[] abc = {"Monday", "Tuesday","Wednusday","Thursday","Friday","Saturday","Sunday"};
         System.out.println("Day is:"+abc[2]);
 
         char[] chr = {'a', 'b', 'c'};
-        System.out.println("char is:"+chr[2]);
+        //System.out.println("char is:"+chr[2]);
+        System.out.println("The char is:"+chr[1]);
     }
 }
