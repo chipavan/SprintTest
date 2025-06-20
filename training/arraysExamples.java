459afcbea45fd902dfe200ba54e4b3b847c0a28a
Pavan Kumar Chiluveru <chipavan@gmail.com>
2025-06-20 20:53:57 +0530

    Added 4 days

--- training/arraysExamples.java
+++ training/arraysExamples.java
@@ -7,7 +7,7 @@ public class arraysExamples {
         int[] a = {21,32,43,34,55,16,87,58,29};         // default array size is 10.
         System.out.println("val..."+a[3]);
 
-        String[] abc = {"Monday", "Tuesday","Wednusday"};
+        String[] abc = {"Monday", "Tuesday","Wednusday","Thursday","Friday","Saturday","Sunday"};
         System.out.println("Day is:"+abc[2]);
 
         char[] chr = {'a', 'b', 'c'};
